53f832707adbe54ad508f1ec5d17ec850487845c
Kerem-Tasan <128915083+Kerem-Tasan@users.noreply.github.com>
2024-03-20 14:12:49 +0300

    Kitap Sıralayıcı
    Patika Collection dersi ödevi Kitap sıralayıcı

--- Main.java
+++ Main.java
@@ -1,5 +1,43 @@
+import java.awt.print.Book;
+import java.util.Comparator;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        TreeSet<Kitap> list=new TreeSet<Kitap>();
+
+
+
+
+          list.add(new Kitap("Kralların Yolu",450));
+          list.add(new Kitap("Demir ALtın",665));
+          list.add(new Kitap("Kızıl Yükseliş",443));
+          list.add(new Kitap("Bilge Adamın Korkusu",1137));
+          list.add(new Kitap("Kızıl Gökler Altında Kızıl Denizler",637));
+          list.add(new Kitap("Rüzgarın Adı",741));
+
+        System.out.println("Kitap adına göre sıralama##");
+        System.out.println("###############");
+
+          for(Kitap i: list){
+              System.out.println(i.getName()+"|"+i.getPage());
+          } System.out.println();
+        System.out.println("##Sayfa sayısına göre sıralama##");
+        System.out.println("###############");
+
+
+
+        TreeSet<Kitap> listbypage=new TreeSet<>(new SayfaComparator());
+        listbypage.addAll(list);
+        for(Kitap i: listbypage){
+            System.out.println(i.getPage()+"|"+i.getName());
+        } System.out.println();
+    }
+}class SayfaComparator implements Comparator<Kitap> {
+    @Override
+    public int compare(Kitap kitap1, Kitap kitap2) {
+        return kitap1.getPage() - kitap2.getPage();
+
     }
 }
